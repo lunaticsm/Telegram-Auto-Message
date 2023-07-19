291b08422e0f2892afa92c8adfcf230925208357
luna <101097846+lunaticsm@users.noreply.github.com>
2023-07-19 20:19:03 +0700

    Update sender.py
    Making it copy not forward

--- sender.py
+++ sender.py
@@ -10,47 +10,59 @@ load_dotenv()
 api_id = os.getenv("API_ID")
 api_hash = os.getenv("API_HASH")
 api_name = os.getenv("API_NAME")
-
 # Account connection
+api_id = 0
+api_hash = 'API_HASH'
+api_name = 'API_NAME'
 client = TelegramClient(api_name, api_id, api_hash)
 
+
 async def main():
+    me = await client.get_me()
+    # Read message from file and send to a list of scraped groups
+
     while True:
-        try:
-            with open("groups.json", 'r') as openfile:
-                json_object = json.load(openfile)
+
+        with open("groups.json", 'r') as openfile:
+            json_object = json.load(openfile)
+
+        for data in json_object:
 
             with open("message.json", "r") as m:
                 msg = json.load(m)
-
-            for data in json_object:
                 random_index = randint(0, len(msg) - 1)
                 message = msg[random_index]
 
-                await client.forward_messages(data['id'], message['msg_id'], message['channel'])
-                print(f"{data['name']} {data['id']} send successfully")
+            try:
+                # Get the original message entity to be forwarded
+                original_msg = await client.get_messages(message['channel'], ids=message['msg_id'])
 
-                time.sleep(10)
+                # Copy the original message to the target group
+                await client.send_message(data['id'], original_msg)
 
-            os.system('cls' if os.name == 'nt' else 'clear')
-            js = []
+                print(data['name'] + " " + str(data['id']) + " message copied successfully")
+            except Exception as e:
+                print(data['name'] + " " + str(data['id']) + " failed to copy message. Reason: " + str(e))
 
-            print("-- UPDATE GRUP LIST --\n")
-            async for dialog in client.iter_dialogs():
-                if str(dialog.id).startswith("-100"):
-                    dt = {"name": dialog.name, "id": dialog.id}
-                    js.append(dt)
-                    print(dialog.name, "has ID", dialog.id)
-
-            with open("groups.json", "w") as outfile:
-                json.dump(js, outfile)
-
-            print("\nwaiting next loop 10 seconds")
             time.sleep(10)
-            os.system('cls' if os.name == 'nt' else 'clear')
 
-        except Exception as e:
-            print("Error:", e)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        js = []
+
+        print("-- UPDATE GROUP LIST --\n")
+        async for dialog in client.iter_dialogs():
+            if str(dialog.id).startswith("-100"):
+                dt = {"name": dialog.name, "id": dialog.id}
+                js.append(dt)
+                print(dialog.name, "has ID", dialog.id)
+
+        with open("groups.json", "w") as outfile:
+            json.dump(js, outfile)
+
+        print("\nWaiting for the next loop in 10 seconds")
+        time.sleep(10)
+        os.system('cls' if os.name == 'nt' else 'clear')
+
 
 with client:
     client.loop.run_until_complete(main())
